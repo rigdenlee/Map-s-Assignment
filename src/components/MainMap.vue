763177a9f55813f77734e0128712c7204343832e
Rigden <rigden.t@enerman.in>
2023-10-26 18:54:33 +0530

    - changes for map component

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -8,18 +8,6 @@
     </div>
     <div :style="{ display: 'flex', justifyContent: 'center' }">
       <div id="map"></div>
-      <div class="card" v-if="selectedFeature">
-        <div class="card-body">
-          <h5 class="card-title">Details</h5>
-          <img
-            class="card-cross"
-            src="../assets/cross.svg"
-            alt="cross"
-            width="30px"
-          />
-          <p class="card-text">{{ selectedFeature }}</p>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -32,7 +20,6 @@ export default {
   data() {
     return {
       map: null,
-      selectedFeature: null,
     };
   },
   mounted() {
@@ -45,111 +32,175 @@ export default {
       zoom: 3,
     });
 
+    this.map.addControl(new mapboxgl.FullscreenControl());
     this.loadPointDataLayer(); // Load the Point data layer
-    this.loadPolygonDataLayer(); // Load the Polygon data layer
-
-    // Add click event listener to the map
-    this.map.on("click", this.onMapClick);
-    fetch("http://localhost:3000/us-churches")
-    .then((response) => {
-      // Check if the response status is OK (status code 200)
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Use the obtained coordinates as needed
-      for (const obj of data) {
-        if (obj.LONGITUDE != undefined && obj.LATITUDE != undefined) {
-          new mapboxgl.Marker({ color: "black", rotationAlignment: "horizon" })
-            .setLngLat([obj.LONGITUDE, obj.LATITUDE])
-            .addTo(this.map);
-        }
-      }
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
+    this.loadBorderLayer(); // Load the Polygon data layer
+    this.loadPolygonDataLayer();
   },
   methods: {
-    loadPointDataLayer() {
-      // Fetch and add Point data to the map
-      // Replace with your API call and rendering logic
+    async loadPointDataLayer() {
+      try {
+        const response = await fetch("http://localhost:3000/us-churches");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+
+        for (const obj of data) {
+          if (obj.LONGITUDE !== undefined && obj.LATITUDE !== undefined) {
+            const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+              `<h3 class="description-title">Church Details:</h3>
+              <p class="description-detail">Name: ${obj.NAME}</p>
+              <p class="description-detail">ATTENDANCE: ${obj.ATTENDANCE}</p>
+              <p class="description-detail">City: ${obj.CITY}</p>
+              <p class="description-detail">County: ${obj.COUNTY}</p>
+              <p class="description-detail">State: ${obj.STATE}</p>`
+            );
+
+            new mapboxgl.Marker({
+              color: "black",
+              rotationAlignment: "horizon",
+            })
+              .setLngLat([obj.LONGITUDE, obj.LATITUDE])
+              .setPopup(popup)
+              .addTo(this.map);
+          }
+        }
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
     },
-    loadPolygonDataLayer() {
-      // Fetch and add Polygon data to the map
+
+    async loadPolygonDataLayer() {
       let hoveredPolygonId = null;
+      try {
+        const response = await fetch(
+          "https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson"
+        );
 
-      this.map.on("load", () => {
-        this.map.addSource("states", {
-          type: "geojson",
-          data: "https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson",
-        });
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
 
-        // The feature-state dependent fill-opacity expression will render the hover effect
-        // when a feature's hover state is set to true.
-        this.map.addLayer({
-          id: "state-fills",
-          type: "fill",
-          source: "states",
-          layout: {},
-          paint: {
-            "fill-color": "#627BC1",
-            "fill-opacity": [
-              "case",
-              ["boolean", ["feature-state", "hover"], false],
-              1,
-              0.5,
-            ],
-          },
-        });
+        const data = await response.json();
 
-        this.map.addLayer({
-          id: "state-borders",
-          type: "line",
-          source: "states",
-          layout: {},
-          paint: {
-            "line-color": "#627BC1",
-            "line-width": 2,
-          },
-        });
+        this.map.on("load", () => {
+          this.map.addSource("states", {
+            type: "geojson",
+            data: data,
+          });
 
-        // When the user moves their mouse over the state-fill layer, we'll update the
-        // feature state for the feature under the mouse.
-        this.map.on("mousemove", "state-fills", (e) => {
-          if (e.features.length > 0) {
+          this.map.addLayer({
+            id: "state-fills",
+            type: "fill",
+            source: "states",
+            layout: {},
+            paint: {
+              "fill-color": "#627BC1",
+              "fill-opacity": [
+                "case",
+                ["boolean", ["feature-state", "hover"], false],
+                1,
+                0.5,
+              ],
+            },
+          });
+
+          this.map.addLayer({
+            id: "state-borders",
+            type: "line",
+            source: "states",
+            layout: {},
+            paint: {
+              "line-color": "#627BC1",
+              "line-width": 2,
+            },
+          });
+
+          this.map.on("mousemove", "state-fills", (e) => {
+            if (e.features.length > 0) {
+              if (hoveredPolygonId !== null) {
+                this.map.setFeatureState(
+                  { source: "states", id: hoveredPolygonId },
+                  { hover: false }
+                );
+              }
+              hoveredPolygonId = e.features[0].id;
+              this.map.setFeatureState(
+                { source: "states", id: hoveredPolygonId },
+                { hover: true }
+              );
+            }
+          });
+
+          this.map.on("mouseleave", "state-fills", () => {
             if (hoveredPolygonId !== null) {
               this.map.setFeatureState(
                 { source: "states", id: hoveredPolygonId },
                 { hover: false }
               );
             }
-            hoveredPolygonId = e.features[0].id;
-            this.map.setFeatureState(
-              { source: "states", id: hoveredPolygonId },
-              { hover: true }
-            );
-          }
+          });
+        });
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
+    },
+
+    async loadBorderLayer() {
+      try {
+        const response = await fetch("http://localhost:3000/us-mexico-boarder");
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        const featureList = data.map(el => {
+          return {
+            type: "Feature",
+            properties: {
+              name: el.properties.gen_type,
+              color: el.properties.barrier === "fence" ? "#ff0000" : "#33FF57",
+            },
+            geometry: {
+              type: "LineString",
+              coordinates: el.geometry.coordinates,
+            },
+          };
         });
 
-        // When the mouse leaves the state-fill layer, update the feature state of the
-        // previously hovered feature.
-        this.map.on("mouseleave", "state-fills", () => {
-          if (hoveredPolygonId !== null) {
-            this.map.setFeatureState(
-              { source: "states", id: hoveredPolygonId },
-              { hover: false }
-            );
-          }
-          hoveredPolygonId = null;
+        const geoJson = {
+          type: "FeatureCollection",
+          features: featureList,
+        };
+
+        this.map.on("load", () => {
+          this.map.addSource("route", {
+            type: "geojson",
+            data: geoJson,
+          });
+
+          featureList.forEach((feature, index) => {
+            this.map.addLayer({
+              id: `route-${index}`,
+              type: "line",
+              source: "route",
+              layout: {
+                "line-join": "round",
+                "line-cap": "round",
+              },
+              paint: {
+                "line-color": feature.properties.color,
+                "line-width": 20,
+              },
+              filter: ["==", "$id", index], // Filter by feature index
+            });
+          });
         });
-      });
-      // Replace with your API call and rendering logic
-    },
-    onMapClick(e) {
-      this.selectedFeature = `Clicked at Longitude: ${e.lngLat.lng}, Latitude: ${e.lngLat.lat}`;
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+      }
     },
   },
 };
@@ -244,5 +295,13 @@ export default {
 
 .card-cross {
   width: 20%;
+}
+
+.description-title {
+  text-align: left;
+}
+
+.description-detail {
+  text-align: left;
 }
 </style>
